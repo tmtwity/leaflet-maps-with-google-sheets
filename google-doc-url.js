55e2553febf5a6f360117617d9abbd66df6bf4e2
tmtwity <30652671+tmtwity@users.noreply.github.com>
2019-06-08 21:51:14 +1200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1DZPbfVByL1Gd40qDpMgFQOFqRpF6_12GZlN1u4ZMHkg/edit#gid=0';
